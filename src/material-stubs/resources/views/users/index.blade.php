b08e57c8b70beb4f158b0093cc379ffc788e210c
Updivision Team <45566836+teamupdivision@users.noreply.github.com>
2020-04-09 11:02:23 +0300

    Update index.blade.php

--- src/material-stubs/resources/views/users/index.blade.php
+++ src/material-stubs/resources/views/users/index.blade.php
@@ -148,9 +148,9 @@
                       <p>{{ __('RTL Support') }}</p>
                     </a>
                   </li>
-                  <li class="nav-item active-pro">
-                    <a class="nav-link" href="{{ route('upgrade') }}">
-                      <i class="material-icons">unarchive</i>
+                  <li class="nav-item active-pro{{ $activePage == 'upgrade' ? ' active' : '' }}">
+                    <a class="nav-link text-white bg-danger" href="{{ route('upgrade') }}">
+                      <i class="material-icons text-white">unarchive</i>
                       <p>{{ __('Upgrade to PRO') }}</p>
                     </a>
                   </li>
